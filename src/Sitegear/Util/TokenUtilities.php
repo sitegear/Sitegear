2e2c2b8acc5ef9bc176d473aba59ca6e451f2ab7
Ben New <ben@leftclick.com.au>
2013-02-20 17:12:44 +0800

    Documentation and allow non-string-convertible values.

--- src/Sitegear/Util/TokenUtilities.php
+++ src/Sitegear/Util/TokenUtilities.php
@@ -8,8 +8,21 @@
 
 namespace Sitegear\Util;
 
+/**
+ * Utilities for dealing with strings containing %tokens%, like this:
+ *
+ * "This is a %foo% with 2 %bar%."
+ */
 class TokenUtilities {
 
+	/**
+	 * Find the tokens present in the given text.  For the example given in the class docs, the result of this would
+	 * be `array( 'foo', 'bar' )`.
+	 *
+	 * @param string $text Text to look for tokens in.
+	 *
+	 * @return array Array of tokens; an empty array if no tokens were found.
+	 */
 	public static function findTokens($text) {
 		$matches = array();
 		preg_match_all('/%([a-z][a-zA-Z0-9]*?)%/', $text, $matches);
@@ -17,9 +30,23 @@ class TokenUtilities {
 		return sizeof($matches) > 1 ? array_unique($matches[1]) : array();
 	}
 
-	public static function replaceTokens($text, $values) {
+	/**
+	 * Replace tokens in the given text using values from the given values array.  For the example given in the class
+	 * docs, one appropriate `$values` array would be `array( 'foo' => 'sentence', 'bar' => 'tokens' )`.
+	 *
+	 * @param string $text Text to replace tokens in.
+	 * @param array $values Flat (non-nested) key-value array where the keys correspond to the tokens in the input
+	 *   text (minus the % delimiters).  Additional keys are allowed and will not cause an error.  Non-string and
+	 *   non-string-convertible values will also be ignored.
+	 *
+	 * @return mixed
+	 */
+	public static function replaceTokens($text, array $values) {
 		foreach ($values as $token => $value) {
-			$text = str_replace(sprintf('%%%s%%', $token), $value, $text);
+			// TODO Is this the best logic for detecting string-convertible values?
+			if (is_string($value) || is_int($value) || is_float($value) || is_bool($value) || (is_object($value) && method_exists($value, '__toString'))) {
+				$text = str_replace(sprintf('%%%s%%', $token), strval($value), $text);
+			}
 		}
 		return $text;
 	}
