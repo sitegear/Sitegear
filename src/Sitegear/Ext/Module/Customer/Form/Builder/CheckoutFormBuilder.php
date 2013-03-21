786ea54b545eb6f7bd308e13bcdb1cb1eb4e1724
Ben New <ben@leftclick.com.au>
2013-03-22 01:50:59 +0800

    Add processors to checkout form builder.

--- src/Sitegear/Ext/Module/Customer/Form/Builder/CheckoutFormBuilder.php
+++ src/Sitegear/Ext/Module/Customer/Form/Builder/CheckoutFormBuilder.php
@@ -36,6 +36,7 @@ class CheckoutFormBuilder extends FormBuilder {
 	 * {@inheritDoc}
 	 */
 	public function buildForm($formDefinition) {
+		// Create the form object.
 		$form = new Form(
 			$this->getSubmitUrl(isset($formDefinition['form-url']) ? $formDefinition['form-url'] : null),
 			$formDefinition['target-url'],
@@ -45,6 +46,7 @@ class CheckoutFormBuilder extends FormBuilder {
 			null,
 			array( 'value' => '< Back' )
 		);
+		// Create fields.
 		foreach ($formDefinition['fields'] as $name => $fieldDefinition) {
 			$field = $this->buildField($name, $fieldDefinition);
 			if (!is_null($fieldValue = $this->account->getNamedFieldValue($name))) {
@@ -52,16 +54,31 @@ class CheckoutFormBuilder extends FormBuilder {
 			}
 			$form->addField($field);
 		}
+		// Create steps.
 		foreach ($formDefinition['steps'] as $stepIndex => $stepDefinition) {
+			// Create the step object.
 			$oneWay = isset($stepDefinition['one-way']) ? $stepDefinition['one-way'] : false;
 			$heading = isset($stepDefinition['heading']) ? $stepDefinition['heading'] : null;
 			$errorHeading = isset($stepDefinition['error-heading']) ? $stepDefinition['error-heading'] : null;
 			$step = new Step($form, $stepIndex, $oneWay, $heading, $errorHeading);
+			// Add fieldsets to the step.
 			foreach ($stepDefinition['fieldsets'] as $fieldsetName) {
 				$fieldsetDefinition = $formDefinition['fieldsets'][$fieldsetName];
 				$step->addFieldset($this->buildFieldset($step, $fieldsetDefinition));
 			}
-			// TODO Processors
+			// Add processors to the step.
+			if (isset($stepDefinition['processors'])) {
+				foreach ($stepDefinition['processors'] as $processorDefinition) {
+					if (is_string($processorDefinition)) {
+						$processorDefinition = array(
+							'module' => 'customer',
+							'method' => $processorDefinition
+						);
+					}
+					$step->addProcessor($this->buildProcessor($processorDefinition));
+				}
+			}
+			// Add step to the form.
 			$form->addStep($step);
 		}
 		return $form;
