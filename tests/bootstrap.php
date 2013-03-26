d825f6ab2b01d4e97833b7ca4fa1cb5baece214e
Ben New <ben@leftclick.com.au>
2013-03-26 16:22:53 +0800

    Improve unit test bootstrap.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,13 +6,35 @@
  * http://sitegear.org/
  */
 
-// Load the autoload from the vendor/ directory or from the parent project.
-if (file_exists($loader = dirname(__DIR__) . '/vendor/autoload.php')) {
-	/** @noinspection PhpIncludeInspection */
-	$loader = require_once $loader;
-} elseif (file_exists($loader = dirname(dirname(dirname(__DIR__))) . '/autoload.php')) {
-	/** @noinspection PhpIncludeInspection */
-	$loader = require_once $loader;
-}
+/**
+ * Sitegear unit test bootstrap.
+ */
+return call_user_func(function() {
 
-$loader->add('Sitegear\\', __DIR__);
+	// Autoloader paths to try.  The last path here is totally non-portable but works in my dev environment so meh.
+	$paths = array(
+		dirname(__DIR__) . '/vendor/autoload.php',
+		dirname(dirname(dirname(__DIR__))) . '/autoload.php',
+		dirname(dirname(dirname(__DIR__))) . '/workspace/testdrive.sitegear.org/vendor/autoload.ph'
+	);
+
+	// Get the loader from the first valid path.
+	$loader = null;
+	while (!empty($paths) && is_null($loader)) {
+		$path = array_shift($paths);
+		if (file_exists($path)) {
+			/** @noinspection PhpIncludeInspection */
+			$loader = require_once $path;
+		}
+	}
+
+	// Stop now if we don't have a valid loader.
+	if (is_null($loader)) {
+		error_log(sprintf('*** FATAL ERROR: Could not find autoloader in unit test bootstrap.  Try adding a valid path to the array in %s ***', __FILE__));
+		exit;
+	}
+
+	// Add the Sitegear prefix for test classes.
+	$loader->add('Sitegear\\', __DIR__);
+
+});
